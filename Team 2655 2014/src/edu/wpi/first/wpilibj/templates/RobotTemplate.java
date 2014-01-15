c8f7fa7993a3d21efa837f7f9eab15284c3dda2f
clonetrooper67 <number45zephan@gmail.com>
2014-01-14 20:26:54 -0500

    Blank version
    Here is the first and empty version of the robot code

---
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -0,0 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj.templates;
+
+
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+
+public class RobotTemplate extends IterativeRobot {
+
+    int ni;
+    
+    public void robotInit() {
+
+    }
+
+
+    public void autonomousPeriodic() {
+
+    }
+
+
+    public void teleopPeriodic() {
+        
+        
+        
+    }
+    
+
+    public void testPeriodic() {
+    
+    }
+    
+}
