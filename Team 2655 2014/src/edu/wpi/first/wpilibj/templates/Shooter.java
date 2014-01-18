89472cd011726fc16580858b5bacd8b24fa4c1c5
magicknees <magicknees@gmail.com>
2014-01-18 16:59:15 -0500

    fixed compile problems

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/Shooter.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/Shooter.java
@@ -3,13 +3,15 @@ package edu.wpi.first.wpilibj.templates;
 // Author Alex Senneville
 
 public class Shooter {
-    Shooter shooter;
+    
    
     public Shooter() {
-        shooter = new Shooter();
+      
     }
      void shoot() {
-            shooter.shoot();
+          
         }
-    
+    void reload() {
+        
+    }
 }
