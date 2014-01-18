9ec5ac21cc69ccd3c75543886ef6f33f9a99fe91
SethBrannan <sethdavidbrannan@gmail.com>
2014-01-18 12:28:41 -0500

    Edited hardware ports

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/PublicEnumHardwarePorts.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/PublicEnumHardwarePorts.java
@@ -5,13 +5,15 @@ import edu.wpi.first.wpilibj.RobotDrive;
 
 public class PublicEnumHardwarePorts {
     
-    public static int lifter1maxPWM = 1; 
-    public static int lifter2maxPWM = 2;
+    public static int anchorDropChannel = 1; 
+    public static int anchorRaiseChannel = 2;
+    
     public static int limitSwitch1PWM= 3;   
     public static int ultraSonicSensorPWM = 4;
     public static int rightArmPWM = 6;   
     public static int leftArmPWM = 7;
-    public static int BallShooterPWM = 5;
+    
+    
     public static int encoder1Channel2 = 6;
     public static int encoder2Channel1 = 7;
     public static int encoder2Channel2 = 8;
