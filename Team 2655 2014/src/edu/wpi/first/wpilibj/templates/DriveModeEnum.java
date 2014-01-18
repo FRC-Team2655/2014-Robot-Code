7230a32066bc2e8b1cd92340978850e2fce5f47a
bionicmuffin <bennettlewis@Bennetts-MacBook-Pro.local>
2014-01-18 14:37:43 -0500

    Now the drive system class is set up to take input from the the Robot Template about what drive mode to be in

---
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/DriveModeEnum.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author bennettlewis
+ */
+public class DriveModeEnum {
+
+    public final static int Disabled = 0;
+    public final static int Autonomous = 1;
+    public final static int Teleop = 2;
+}
--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/DriveSystem.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/DriveSystem.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.Gyro;
@@ -11,42 +10,51 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 
 public class DriveSystem implements Runnable {
-    
-    
-    Joystick driveStick; 
+
+    Joystick driveStick;
     Gyro gyro;
     public static int useGyro = 1;
-    
-    RobotDrive mainDrive = new RobotDrive(1,2,3,4); //change motors later
-    
-    
+
+    RobotDrive mainDrive = new RobotDrive(1, 2, 3, 4); //change motors later
+
+    int driveMode;
+
     public DriveSystem(Joystick driveStick) {
         this.driveStick = driveStick;
         this.gyro = gyro;
         gyro.reset();
+        driveMode = DriveModeEnum.Disabled;
+
     }
-   
+
     //should we add a reset button for the gyro?
     //should we also add another button to automatically point to zero
     public void run() {
-        
-        
-        while(true){ 
+
+        while (true) {
             try {
-                //make a sleep mode in the smart dash? should the thread sleep?
-                //create and if statement to determine if we are in teleop or auto
+
                 mainDrive.mecanumDrive_Cartesian(driveStick.getAxis(Joystick.AxisType.kX),
                         driveStick.getAxis(Joystick.AxisType.kY),
                         driveStick.getAxis(Joystick.AxisType.kZ),
                         gyro.getAngle() * useGyro);
+
                 Thread.sleep(1);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
-       
+
         }
     }
 
-    
+    public void setDriveMode(int mode) {
+        if (mode < DriveModeEnum.Disabled) {
+
+        } else if (mode > DriveModeEnum.Teleop) {
+            
+        } else {
+            driveMode = mode;
+        }
+    }
 
 }
