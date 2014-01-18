55b61f548beb5f50223b227afb28ed2f90a66356
sexytacoman99 <alex.senneville1@gmail.com>
2014-01-18 11:46:13 -0500

    almost finished infeed class

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/InFeed.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/InFeed.java
@@ -19,12 +19,19 @@ public class InFeed {
     }
 
     void extend() {
-        
+        leftArmMotorControl.set(Relay.Value.kOn);
+        leftArmMotorControl.set(Relay.Value.kForward);
+        rightArmMotorControl.set(Relay.Value.kOn);
+        rightArmMotorControl.set(Relay.Value.kForward);
+        liftArmControl.extend();
             //spin motors
         //put arms down
     }
 
     void retract() {
+        leftArmMotorControl.set(Relay.Value.kOff);
+        rightArmMotorControl.set(Relay.Value.kOff);
+        liftArmControl.retract();
             //turn the motors off 
         //retract arms
     }
