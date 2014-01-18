fea9ea7e0292fb94bec37118f2d32dc280eb72d1
sexytacoman99 <alex.senneville1@gmail.com>
2014-01-18 10:38:14 -0500

    created motor for ball handler class

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/motorForBallHandler.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/motorForBallHandler.java
@@ -1,19 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package edu.wpi.first.wpilibj.templates;
+    // Author Alex Senneville
 
-/**
- *
- * @author tronage gaming
- */
 public class motorForBallHandler {
+    motorForBallHandler motorForBallHHandler;
 
     public motorForBallHandler() {
+        motorForBallHandler motorForBallHandler = new motorForBallHandler();
     }
     
+    void start(){
+    
+}
+    void stop(){
+        
+    }
     
 }
