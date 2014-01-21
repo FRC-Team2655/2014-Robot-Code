f244846553bced78e4dde950c851f77f3aa4ba1d
sexytacoman99 <alex.senneville1@gmail.com>
2014-01-20 20:37:27 -0500

    rough draft of shooter class

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/Shooter.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/Shooter.java
@@ -1,26 +1,48 @@
-
 package edu.wpi.first.wpilibj.templates;
 // Author Alex Senneville
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
-
 public class Shooter {
 
-    DoubleSolenoid shooterPistons;
-   
+    DoubleSolenoid shooterPiston1;
+    DoubleSolenoid shooterPiston2;
+
     public Shooter() {
-        
+        shooterPiston1 = new DoubleSolenoid(HardwarePortsEnum.ShooterExtendChannel, HardwarePortsEnum.ShooterRetractChannel);
+        //may need to change channels and may need to add spike to conttrol multiple channels
+        shooterPiston2 = new DoubleSolenoid(HardwarePortsEnum.ShooterExtendChannel, HardwarePortsEnum.ShooterRetractChannel);
     }
-     void shoot() {
-           
+
+    void shoot() {
+        shooterPiston1.set(DoubleSolenoid.Value.kForward);
+        shooterPiston2.set(DoubleSolenoid.Value.kForward);
+        //after the ball is shot we might need a timer in there to make the piston extend
+        //wait until piston completly extends 
+        try {
+            wait(100);
+        } catch (Exception e) {
+
+        } finally {
+            shooterPiston2.set(DoubleSolenoid.Value.kOff);
+            shooterPiston1.set(DoubleSolenoid.Value.kOff);
         }
-     void reset() {
-         
-     }
-         
-         
+
+    }
+
+    void reset() {
+        shooterPiston1.set(DoubleSolenoid.Value.kReverse);
+        shooterPiston2.set(DoubleSolenoid.Value.kReverse);
+        //wait until piston completly retracts wait(timeout);
+        try {
+            wait(100);
+        } catch (Exception e) {
+
+        } finally {
+            shooterPiston2.set(DoubleSolenoid.Value.kOff);
+            shooterPiston1.set(DoubleSolenoid.Value.kOff);
+        }
+
+    }
+
 }
-    
-   
-    
