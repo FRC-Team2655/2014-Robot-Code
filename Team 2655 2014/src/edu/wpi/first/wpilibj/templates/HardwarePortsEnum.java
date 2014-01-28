27924d70f15cb94c939c6b52f0b5ea57a61983a6
clonetrooper67 <number45zephan@gmail.com>
2014-01-27 19:26:07 -0500

    HardwarePorts Changes

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/HardwarePortsEnum.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/HardwarePortsEnum.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.wpi.first.wpilibj.templates;
 
 /**
@@ -12,42 +11,37 @@ package edu.wpi.first.wpilibj.templates;
  */
 public class HardwarePortsEnum {
 //  Hardware ports are incorrect and/or temporary.
-   
-    public static int anchorDropChannel = 1; 
-    public static int anchorRaiseChannel = 2;
-    // no. 3 goes here
-    // no. 4 goes here
-    //  no. 5 goes here
-    
-    public static int pressureSwitchChannel = 6;
-    public static int compressorRelayChannel = 7; 
-    
-    public static int leftArmMotorControlChannel = 8;
-    public static int rightArmMotorControlChannel = 9;
-  
 
-    public static int sideArmOpenArmChannel = 10;
-    public static int sideArmClosedArmChannel = 11;
+    public static int frontLeftMotorChannel = 1;
+    public static int backLeftMotorChannel = 2;
+    public static int frontRightMotorChannel = 3;
+    public static int backRightMotorChannel = 4;
     
-    public static int leftRangeFinderEchoPort = 12;
-    public static int leftRangeFinderPingPort = 13;
-    
-    public static int rightRangeFinderEchoPort = 17;
-    public static int rightRangeFinderPingPort = 18;
-    
-    public static int ballInMittLimitSwitchChannel = 14;
-   
-    public static int shooterExtendChannel = 15;
-    public static int shooterRetractChannel = 16;
-    
-    public static int gyroChannel = 17;
-    
-    public static int frontLeftMotorChannel = 18;
-    public static int backLeftMotorChannel = 19;
-    public static int frontRightMotorChannel = 20;
-    public static int backRightMotorChannel = 21;
-    
-    public static int infeedArmMotorControlChannel = 22;
-    
-    
+    public static int anchorDropChannel = 5;
+    public static int anchorRaiseChannel = 6;
+
+    public static int pressureSwitchChannel = 7;
+    public static int compressorRelayChannel = 8;
+
+    public static int leftArmMotorControlChannel = 9;
+    public static int rightArmMotorControlChannel = 10;
+
+    public static int sideArmOpenArmChannel = 11;
+    public static int sideArmClosedArmChannel = 12;
+
+    public static int leftRangeFinderEchoPort = 13;
+    public static int leftRangeFinderPingPort = 14;
+
+    public static int rightRangeFinderEchoPort = 15;
+    public static int rightRangeFinderPingPort = 16;
+
+    public static int ballInMittLimitSwitchChannel = 17;
+
+    public static int shooterExtendChannel = 18;
+    public static int shooterRetractChannel = 19;
+
+    public static int gyroChannel = 20;
+
+    public static int infeedArmMotorControlChannel = 21;
+
 }
