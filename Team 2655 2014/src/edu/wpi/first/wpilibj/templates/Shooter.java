1b4a207a97d34b36f199f25fd27a23f852bd7f79
sexytacoman99 <alex.senneville1@gmail.com>
2014-02-01 12:05:02 -0500

    modoified shooter class

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/Shooter.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/Shooter.java
@@ -1,36 +1,36 @@
-
 package edu.wpi.first.wpilibj.templates;
 // Author Alex Senneville
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Shooter {
+
     //keep in mind that i might need to refactor names
     DoubleSolenoid shooterPiston1;
-   // DoubleSolenoid shooterPiston2;
+    // DoubleSolenoid shooterPiston2;
     //change these later to the right time
     int waitTimeShoot = 1000;
     int waitTimePass = 100;
     int timeForShooterToRetract = 250;
-    
+
     public Shooter() {
         shooterPiston1 = new DoubleSolenoid(Ports.SolenoidModule.shooterExtendChannel, Ports.SolenoidModule.shooterRetractChannel);
         //shooterPiston2 = new DoubleSolenoid(Ports.SolenoidModule.shooterExtendChannel, Ports.SolenoidModule.shooterRetractChannel);
         //may need to change channels and may need to add spike to conttrol multiple channels
     }
 
-    void shoot() {
-       
-        shooterPiston1.set(DoubleSolenoid.Value.kForward);      
-       // shooterPiston2.set(DoubleSolenoid.Value.kForward);      
+    public  void shoot() {
+
+        shooterPiston1.set(DoubleSolenoid.Value.kForward);
+        // shooterPiston2.set(DoubleSolenoid.Value.kForward);      
         try {
             wait(waitTimeShoot);// Wait until piston completly extends
         } catch (Exception e) {
 
         } finally {
-             shooterPiston1.set(DoubleSolenoid.Value.kReverse);
+            shooterPiston1.set(DoubleSolenoid.Value.kReverse);
             // shooterPiston2.set(DoubleSolenoid.Value.kReverse);
-             //can leave this in but remember in the future it can be in the catch itself
+            //can leave this in but remember in the future it can be in the catch itself
         }
 
         // Wait until piston completly retracts.
@@ -39,45 +39,35 @@ public class Shooter {
         } catch (Exception e) {
 
         } finally {
-             shooterPiston1.set(DoubleSolenoid.Value.kOff);
+            shooterPiston1.set(DoubleSolenoid.Value.kOff);
             // shooterPiston2.set(DoubleSolenoid.Value.kOff);
-       }
+        }
 
     }
-    
-    void pass(){
-         shooterPiston1.set(DoubleSolenoid.Value.kForward);      
+
+    void pass() {
+        shooterPiston1.set(DoubleSolenoid.Value.kForward);
         //shooterPiston2.set(DoubleSolenoid.Value.kForward);      
 //  Will add code later. Should be similar to shoot.
 //change the wait time for both
-         try {
+        try {
             wait(waitTimePass);// Wait until piston completly extends
         } catch (Exception e) {
 
         } finally {
-             shooterPiston1.set(DoubleSolenoid.Value.kReverse);
+            shooterPiston1.set(DoubleSolenoid.Value.kReverse);
           //   shooterPiston2.set(DoubleSolenoid.Value.kReverse);
-             //can leave this in but remember in the future it can be in the catch itself
+            //can leave this in but remember in the future it can be in the catch itself
         }
-        
-         try {
+
+        try {
             wait(timeForShooterToRetract);
         } catch (Exception e) {
 
         } finally {
-             shooterPiston1.set(DoubleSolenoid.Value.kOff);
-          //   shooterPiston2.set(DoubleSolenoid.Value.kOff);
-       }
-    
+            shooterPiston1.set(DoubleSolenoid.Value.kOff);
+            //   shooterPiston2.set(DoubleSolenoid.Value.kOff);
+        }
+
     }
 }
-
-        
-   
-         
-         
-
-    
-   
-    
-
