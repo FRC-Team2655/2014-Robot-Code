64619bdb1138799dad910fc478a32e6731d272df
magicknees <magicknees@gmail.com>
2014-02-01 13:41:38 -0500

    new class to replace DoubleSolenoid
    first checkin

---
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/TeamDoubleSolenoid.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.DigitalOutput;
+
+/**
+ *
+ * @author magic_000
+ */
+public class TeamDoubleSolenoid {
+
+    DigitalOutput forward;
+    DigitalOutput reverse;
+
+ public static class Value {
+
+        public final int value;
+        public static final int kOff_val = 0;
+        public static final int kForward_val = 1;
+        public static final int kReverse_val = 2;
+        public static final Value kOff = new Value(kOff_val);
+        public static final Value kForward = new Value(kForward_val);
+        public static final Value kReverse = new Value(kReverse_val);
+
+        private Value(int value) {
+            this.value = value;
+        }
+    }
+
+    public TeamDoubleSolenoid(int module, int forwardChannel, int reverseChannel) {
+        forward = new DigitalOutput(module, forwardChannel);
+        reverse = new DigitalOutput(module, reverseChannel);
+    }
+
+    public void set(final Value value) {
+        switch (value.value) {
+            case Value.kOff_val:
+                forward.set(false);
+                reverse.set(false);
+                break;
+                
+            case Value.kForward_val:
+                forward.set(true);
+                reverse.set(false);
+                break;
+
+            case Value.kReverse_val:
+                forward.set(false);
+                reverse.set(true);
+                break;
+            default:
+        }
+    }
+
+}
