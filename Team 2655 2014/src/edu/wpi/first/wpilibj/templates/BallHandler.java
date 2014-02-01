9cbad02ffd0a0f7e86c8691d6cf58d4db6a95e68
SethBrannan <sethdavidbrannan@gmail.com>
2014-02-01 15:45:56 -0500

    worked on state machine

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/BallHandler.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/BallHandler.java
@@ -25,6 +25,7 @@ public class BallHandler {
         public static final int off = 0;
         public static final int loading = 1;
         public static final int closing = 2;
+        public static final int loaded = 3;
 
     }
 
@@ -41,13 +42,25 @@ public class BallHandler {
                     case loadStates.loading:
                         sideArm.open();
                         inFeed.on();
+                        
+                        loadMode = loadStates.off;
+                        loadIsEnabled = true;
                         break;
+                        
                     case loadStates.closing:
                         sideArm.close();
                         inFeed.off();
+                        
+                        loadMode = loadStates.off;
+                        loadIsEnabled = true;
                         break;
+                        
+                    case loadStates.loaded:
+                        
                     case loadStates.off:
+                        loadIsEnabled = false;
                         break;
+                        
                     default:
                         break;
 
