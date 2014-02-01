0b94fab61d6f7212702499dacaa3fcd4808e496b
SethBrannan <sethdavidbrannan@gmail.com>
2014-02-01 15:49:02 -0500

    Worked on the state machine

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/BallHandler.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/BallHandler.java
@@ -56,6 +56,8 @@ public class BallHandler {
                         break;
                         
                     case loadStates.loaded:
+                        loadIsEnabled = true;
+                        break;
                         
                     case loadStates.off:
                         loadIsEnabled = false;
