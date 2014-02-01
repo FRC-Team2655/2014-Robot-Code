f86e33ea55614a7697bf4cab62a40819a6ed8842
SethBrannan <sethdavidbrannan@gmail.com>
2014-01-31 19:22:39 -0500

    Bug fixes for anchor class

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/Anchor.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/Anchor.java
@@ -13,34 +13,31 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
  * @author Seth - edited by Zephan
  */
 public class Anchor {
-    
-    private DoubleSolenoid anchors;
-    
-//  You will most likely need a timer.
 
+    private DoubleSolenoid anchors;
+
+//  You will most likely need a timer.
     public Anchor() {
-        
+
         anchors = new DoubleSolenoid(Ports.SolenoidModule.anchorDropChannel, Ports.SolenoidModule.anchorRaiseChannel);
         //ballInMittLimitSwitch = new DigitalInput(1);
 //      anchors.set(DoubleSolenoid.Value.kOff); - Double checking if anchors are off?
 
     }
-    
+
     void drop() {
-        
+
         anchors.set(DoubleSolenoid.Value.kForward);
     }
-    
+
     void raise() {
-        
+
         anchors.set(DoubleSolenoid.Value.kReverse);
         try {
             wait(Global.anchorRaiseTime);
             //Find if we need to stop the solenoid when it reaches th top position.
         } catch (InterruptedException ex) {
-            if (debugRaiseException) {
-                ex.printStackTrace();
-            }
+
         }
         anchors.set(DoubleSolenoid.Value.kOff);
     }
