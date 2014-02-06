4746f5ebd1258e0cdaaa9eab79292e874c0525a8
SethBrannan <sethdavidbrannan@gmail.com>
2014-02-05 21:06:24 -0500

    started working on new infeed

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/InFeed.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/InFeed.java
@@ -2,16 +2,28 @@ package edu.wpi.first.wpilibj.templates;
 
 // Author Zephan
 import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class InFeed {
 
+    Solenoid inFeedSolenoid;
     Relay infeedArmMotorControl;
+    
+    public int forward = 1;
+    public int reverse = 2;
 
     public InFeed() {
 //  Make sure to change relays and channels later.
 
         infeedArmMotorControl = new Relay(Ports.crioSlot2,Ports.DigitalModule.infeedArmMotorControlChannel);
 
+    }
+    
+    void pass() {
+        
+        //inFeedSolenoid.set(true);
+        infeedArmMotorControl.set(Relay.Value.kReverse);
+        
     }
 
     void on() {
