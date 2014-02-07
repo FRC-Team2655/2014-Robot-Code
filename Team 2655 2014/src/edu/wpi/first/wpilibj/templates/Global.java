6844bd1fc509fc8431e4ebe04a14775998c8e0e6
SethBrannan <sethdavidbrannan@gmail.com>
2014-02-06 20:30:34 -0500

    global variable timer added

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/Global.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/Global.java
@@ -13,8 +13,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 // MUST CHANGE THE VALUES
 public class Global {
+//  Variables used in load arm
+    public static long loadArmRaiseTime = 100;
+    
 //  This number is used when the moveTo method calculates the speed that the robot needs to move at.  
-
     public static double speedSlopeMoving = 0.2;         // m
     public static double speedSlopeRotate = 0.00555555;  // m
 
@@ -32,38 +34,38 @@ public class Global {
     public static int loadIdleTime = 100;
     public static int catchIdleTime = 100;
 
-//anchor raise and drop times
+//  anchor raise and drop times
     public static int anchorDropTime = 600;
     public static int anchorRaiseTime = 600;
 
-// drive system 
+//  drive system 
     public static int distanceLimitPositive = 5;
     public static int distanceLimitNegative = -5;
 
-// side arms
+//  side arms
     public static int sideArmCloseTime = 100;
     public static int sideArmOpenTime = 100;
     public static int sideArmIdleTime = 100;
 
-// Gyro
+//  Gyro
     public static double kDefaultVoltsPerDegreePerSecond = .007;// gyro sensitivity
 
-// Temperature slope m = (9mV/C)
-    // y = mx + b
+//  Temperature slope m = (9mV/C)
+//  y = mx + b
     public static double temperatureSlopeCompensation = 111.1111; // m
     public static double temperatureOffset = (-252.7777);         // b
 
-// RangeFinder
-    //public static double voltagePerInch = 512;
+//  RangeFinder
+//  public static double voltagePerInch = 512;
     public static double convertMultiplier = 104.16; // m
 
-// Joystick buttons
+//  Joystick buttons
     public static int shootButton = 1;
     public static int anchorButton = 3;
     public static int loadButton = 4;
     public static int poopButton = 5;
 
-// Smart Dashboard 
+//  Smart Dashboard 
     public static void smartDashBoardGlobalVariables() {
         SmartDashboard.putNumber("speed Slope Moving", speedSlopeMoving);
         SmartDashboard.putNumber("speed Slope Rotate", speedSlopeRotate);
