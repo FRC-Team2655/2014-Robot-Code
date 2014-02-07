ba418b664a707f3f6cc403e4bf8600a34e5827a2
magicknees <magicknees@gmail.com>
2014-02-06 20:24:57 -0500

    cleanup

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/BallHandler.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/BallHandler.java
@@ -12,7 +12,6 @@ public class BallHandler {
     Anchor anchor;
     InFeed inFeed;
     DigitalInput ballInMittLimitSwitch;
-    //DigitalInput shooterLimiterSwitch;
 
     int timesTriedToActivate;
     boolean m_anchorIsDropped;
@@ -20,19 +19,19 @@ public class BallHandler {
     Thread m_thread;
 
     public void BallHandler() {
-        m_anchorIsDropped = false;
+
         //if we ever add or modify a timer do it in the  class itself
         ballHandlerCompressor = new CompressorSystem();
         shooter = new Shooter();
         sideArm = new SideArms();
         anchor = new Anchor();
         inFeed = new InFeed();
+
         ballInMittLimitSwitch = new DigitalInput(Ports.DigitalModule.ballInMittLimitSwitchChannel);
+        m_anchorIsDropped = false;
+        ballHandlerCompressor.start();
 
         timesTriedToActivate = 0;
-        //shooterLimiterSwitch = new DigitalInput(Ports.DigitalModule.shooterLimiterSwitchChannel);
-
-        ballHandlerCompressor.start();
     }
 
     public void catchEnable() {
@@ -42,10 +41,8 @@ public class BallHandler {
         if (m_thread.isAlive()) {
             return;
         }
-
         m_thread = new Thread(new LoadAndCatchCommand(sideArm, ballInMittLimitSwitch, inFeed));
         m_thread.start();
-
     }
 
     public void passTheBall() {
@@ -80,14 +77,10 @@ public class BallHandler {
         if (!m_thread.isAlive()) {
             return;
         }
-        //Experiment
         m_thread.interrupt();
-        //m_thread.notifyAll();
     }
 
     public boolean catchIsEnabled() {
-        //test this
-        //m_thread.equals(this);
         return m_thread.isAlive();
     }
 
@@ -101,28 +94,21 @@ public class BallHandler {
 
         m_thread = new Thread(new LoadAndCatchCommand(sideArm, ballInMittLimitSwitch, inFeed));
         m_thread.start();
-//        loadState = loadStates.loading;
     }
 
     public void loadDisable() {
         if (!m_thread.isAlive()) {
             return;
         }
-        //Experiment with these two methods.
         m_thread.interrupt();
-        //m_thread.notifyAll();
     }
 
     public boolean loadIsEnabled() {
-        //test this
-        //m_thread.equals(this);
         return m_thread.isAlive();
-
     }
 
     public void dropAnchor() {
         anchor.drop();
-        
     }
 
     public void raiseAnchor() {
