71aa7a14e170640e051dc305985c1e5e6059078a
clonetrooper67 <number45zephan@gmail.com>
2014-02-08 14:14:18 -0500

    derp

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/InFeed.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/InFeed.java
@@ -5,45 +5,45 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Relay;
 
 public class InFeed {
-//
-//    private final DoubleSolenoid loadArmLift;
-//    private final Relay infeedArmMotorControl;
-//
-//    public InFeed() {
-////  Make sure to change relays and channels later.
-//
-//        infeedArmMotorControl = new Relay(Ports.DigitalModule.infeedArmMotorControlChannel);
-//        loadArmLift = new DoubleSolenoid(Ports.SolenoidModule2.loadArmExtendChannel, Ports.SolenoidModule2.loadArmRetractChannel);
-//
-//    }
+
+    private final DoubleSolenoid loadArmLift;
+    private final Relay infeedArmMotorControl;
+
+    public InFeed() {
+//  Make sure to change relays and channels later.
+
+        infeedArmMotorControl = new Relay(Ports.DigitalModule.infeedArmMotorControlChannel);
+        loadArmLift = new DoubleSolenoid(Ports.SolenoidModule2.loadArmExtendChannel, Ports.SolenoidModule2.loadArmRetractChannel);
+
+    }
 
     void on() {
 //  Turns the motors on in the foward direction which should pull the ball in.    
 
-//        loadArmLift.set(DoubleSolenoid.Value.kForward);
-//        try {
-//            Thread.sleep(Global.loadArmExtendTime);
-//
-//        } catch (InterruptedException ex) {
-//
-//        }
-//        infeedArmMotorControl.set(Relay.Value.kOn);
-//        loadArmLift.set(DoubleSolenoid.Value.kOff);
+        loadArmLift.set(DoubleSolenoid.Value.kForward);
+        try {
+            Thread.sleep(Global.loadArmExtendTime);
+
+        } catch (InterruptedException ex) {
+
+        }
+        infeedArmMotorControl.set(Relay.Value.kOn);
+        loadArmLift.set(DoubleSolenoid.Value.kOff);
     }
 
     void off() {
 //  Turns the motors off    
 
-//        loadArmLift.set(DoubleSolenoid.Value.kReverse);
-//        try {
-//
-//            Thread.sleep(Global.loadArmRaiseTime);
-//
-//        } catch (InterruptedException ex) {
-//
-//        }
-//        loadArmLift.set(DoubleSolenoid.Value.kOff);
-//        infeedArmMotorControl.set(Relay.Value.kOff);
+        loadArmLift.set(DoubleSolenoid.Value.kReverse);
+        try {
+
+            Thread.sleep(Global.loadArmRaiseTime);
+
+        } catch (InterruptedException ex) {
+
+        }
+        loadArmLift.set(DoubleSolenoid.Value.kOff);
+        infeedArmMotorControl.set(Relay.Value.kOff);
 
     }
 }
