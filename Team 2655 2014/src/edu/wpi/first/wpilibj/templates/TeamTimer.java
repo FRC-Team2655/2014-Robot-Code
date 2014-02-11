d55f470eedbb8f7f51e99935d4b4abd9a5c9c3aa
magicknees <magicknees@gmail.com>
2014-02-10 19:17:56 -0500

    added Timer that works in milliseconds

---
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/TeamTimer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author magic_000
+ */
+public class TeamTimer {
+      public static void delay(final int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (final InterruptedException e) {
+        }
+    }
+}
