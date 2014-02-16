6cf30fba1fe2bdb59d659c67f2e00319eaa43de8
magicknees <magicknees@gmail.com>
2014-02-15 20:16:12 -0500

    use constants

--- Team 2655 2014/src/edu/wpi/first/wpilibj/templates/InFeed.java
+++ Team 2655 2014/src/edu/wpi/first/wpilibj/templates/InFeed.java
@@ -8,8 +8,13 @@ public class InFeed {
 
     private final DoubleSolenoid loadArmLift;
 
+    // two spike relays
     private final Relay leftLoadArmMotor;
     private final Relay rightLoadArmMotor;
+
+    private final DoubleSolenoid.Value DROP_ARMS = DoubleSolenoid.Value.kForward;
+    private final DoubleSolenoid.Value AIR_OFF = DoubleSolenoid.Value.kOff;
+    private final DoubleSolenoid.Value LIFT_ARMS = DoubleSolenoid.Value.kReverse;
 
     private final Relay.Value MOTOR_FORWARD = Relay.Value.kForward;
     private final Relay.Value MOTOR_REVERSE = Relay.Value.kReverse;
@@ -20,36 +25,34 @@ public class InFeed {
 
         leftLoadArmMotor = new Relay(Ports.infeedLeftArmMotorControlChannel);
         rightLoadArmMotor = new Relay(Ports.infeedRightArmMotorControlChannel);
-
     }
 
     // drop the arms
     // turn on the motors
     void on() {
 
-        loadArmLift.set(DoubleSolenoid.Value.kForward); // turn air on to put arms down
+        loadArmLift.set(DROP_ARMS); // turn air on to put arms down
 
         TeamTimer.delay(Global.loadArmExtendTime);
 
-        loadArmLift.set(DoubleSolenoid.Value.kOff); // turn air off
+        loadArmLift.set(AIR_OFF); // turn air off
 
         leftLoadArmMotor.set(MOTOR_FORWARD); // turn motors on in forward direction
         rightLoadArmMotor.set(MOTOR_REVERSE); // turn motors on in forward direction
-
     }
 
     // lift the arms (and ball maybe)
     // turn off the motor
     void off() {
-        
+
         leftLoadArmMotor.set(MOTOR_OFF); // turn motors off
         rightLoadArmMotor.set(MOTOR_OFF);
-        
-        loadArmLift.set(DoubleSolenoid.Value.kReverse); // turn air on to lift arms
+
+        loadArmLift.set(LIFT_ARMS); // turn air on to lift arms
 
         TeamTimer.delay(Global.loadArmRaiseTime);
 
-        loadArmLift.set(DoubleSolenoid.Value.kOff); // turn air off
+        loadArmLift.set(AIR_OFF); // turn air off
 
     }
 }
